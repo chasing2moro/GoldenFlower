6bb241035cab2a659f13bb44df189a32aad7e58d
ballball <730244056@qq.com>
2016-11-18 10:21:34 +0800

    xml 解析

---
+++ GoldenFlower/GoldenFlowerClient/Assets/Script/Framework/CommonShare/XML/Record/RecordRecipe.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public enum RecipeType{
+	Meat,
+	Vegetable,
+	Dessert
+}
+
+[System.Serializable]
+public class RecordRecipe : XMLRecord {
+  
+
+     //子类要实现它
+    public static string GetConfigPath()
+    {
+        return "recipe";
+    }
+
+
+    [XMLAttributeInt]
+    public int id;
+
+    [XMLAttributeString]
+	public string recipename;
+
+	[XMLAttributeInt]
+	public int preptime;
+
+	[XMLAttributeEnum(typeof(RecipeType))]
+	public RecipeType type;
+}
---
+++ GoldenFlower/GoldenFlowerClient/Assets/Script/Framework/CommonShare/XML/XMLParser/XMLRecord.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Reflection;
+using System.Xml;
+using System;
+
+public abstract class XMLRecord
+{
+    //子类要实现它
+    //public static string GetConfigPath()
+    //{
+    //    return null;
+    //}
+
+    //vKeyName 约定为 id
+    public virtual T GetKey<T>(string vKeyName)
+    {
+        FieldInfo fieldInfo = GetType().GetField(vKeyName);
+        object id = fieldInfo.GetValue(this);
+        return (T)Convert.ChangeType(id, typeof(T));
+    }
+
+    public FieldInfo[] GetFieldInfos(){
+		return GetType().GetFields(BindingFlags.Public|
+		                               BindingFlags.NonPublic|
+		                               BindingFlags.Instance|
+		                               BindingFlags.GetProperty);
+	}
+
+	public bool AssignMember(XmlElement vElement){
+		FieldInfo[] fieldInfos = GetFieldInfos();
+		foreach (FieldInfo field in fieldInfos) {
+			XMLAttributeMember[] attributeMembers = (XMLAttributeMember[])field.GetCustomAttributes(typeof(XMLAttributeMember), true);
+			if(attributeMembers.Length > 0){
+				XMLAttributeMember attributeMember = attributeMembers[0];
+				field.SetValue(this, attributeMember.GetData(vElement, field.Name));
+			}
+		}
+		return false;
+	}
+
+}
