35d986f3e2451440132643d6e276adcd1776108f
ballball <730244056@qq.com>
2016-11-23 11:17:19 +0800

    添加状态机

---
+++ GoldenFlower/GoldenFlowerServer/QuickStart/GoldenFlowerServer/CommonShare/Battle/State/StateBet.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/// <summary>
+/// 下注
+/// </summary>
+public class StateBet : StateBase
+{
+    public override void OnEnterState()
+    {
+        throw new NotImplementedException();
+    }
+}
+
---
+++ GoldenFlower/GoldenFlowerServer/QuickStart/GoldenFlowerServer/CommonShare/Battle/State/StateQuit.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/// <summary>
+/// 不跟
+/// </summary>
+public class StateQuit : StateBase
+{
+    public override void OnEnterState()
+    {
+        throw new NotImplementedException();
+    }
+}
+
